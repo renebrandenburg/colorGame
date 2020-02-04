bf6a0db115516c9f2a7c424df98723f28f3eccb0
René Brandenburg <rbrandenburg@drieentachtig.175.surf.nl>
2020-02-04 18:58:05 +0100

    style page

--- color.js
+++ color.js
@@ -49,7 +49,7 @@ resetButton.addEventListener("click", function(){
 	for(var i = 0; i < squares.length; i++){
 		squares[i].style.backgroundColor = colors[i];
 	}
-	h1.style.backgroundColor = "#232323";
+	h1.style.backgroundColor = "steelblue";
 });
 
 colorDisplay.textContent = pickedColor;
@@ -69,7 +69,7 @@ for(var i = 0; i < squares.length; i++){
 			changeColors(clickedColor);
 			h1.style.backgroundColor = clickedColor;
 		} else {
-			this.style.backgroundColor = "#232323";
+			this.style.backgroundColor = "steelblue";
 			messageDisplay.textContent = "Try Again";
 		}
 	});
